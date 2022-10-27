058bb7e6f1cffd81ae6049b60d6c72480178fa24
Islam <islam.qayumov07@gmail.com>
2022-10-27 15:09:31 +0200

    converting comma separated data to array

--- Challenge_#2/main.js
+++ Challenge_#2/main.js
@@ -1,7 +1,15 @@
 // Convert comma-separated data to an Array of string rows
 const csvToRows = (str) => {
+    let output = [];
     
+    for (const row of str.split('\n')) {
+      output.push(row.split(','));
+    }
+
+    return output;
 };
+
+
 
 // Remove duplicate rows from the Array
 const removeDuplicateRows = (arr) => {};
@@ -26,14 +34,12 @@ const findBadEmailIds = (arr) => {
 // Implement the data processing pipeline using the functions above and return an array of user objects with no duplicates, sorted by first_name, timestamps converted to date objects and the user permissions object added.
 const processData = (data) => {};
 
-// PLEASE DO NOT DELETE/MODIFY THE CODE BELOW
-module.exports = {
-  csvToRows,
-  removeDuplicateRows,
-  strRowsToObjects,
-  timeToDate,
-  sortByFirstName,
-  addUserPermissions,
-  findBadEmailIds,
-  processData,
-};
+
+let str = `id,first_name,last_name,email,designation,registeredOn
+1,Kata,Petrollo,Kata@jscorp.co,Budget/Accounting Analyst IV,1586786455
+2,Rafaellle,Francescone,Rafaellle@jscorp.co,Quality Engineer,1591762193
+3,Hort,Shufflebotham,Hort@jscorp.co,Paralegal,1604920379
+4,Raimundo,Beddingham,Raimundo@jscorp.co,Librarian,1590530472
+5,Jamaal,Hyde,Jamaal@jscorp.co,Quality Engineer,1606653657`;
+
+console.table(csvToRows(str));
