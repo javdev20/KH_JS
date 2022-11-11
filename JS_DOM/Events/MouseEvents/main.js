c28d98704d73a39cd0351fd053c7524c6b0157e6
javdev20 <islam.qayumov07@gmail.com>
2022-11-11 17:05:04 +0100

    mouse events

---
+++ JS_DOM/Events/MouseEvents/main.js
@@ -0,0 +1,24 @@
+const allEmployees = document.querySelector('.all-employees');
+const { top, left } = allEmployees.getBoundingClientRect();
+
+const createPanel = (x, y, name) => {
+  const createPanel = document.createElement('div');
+  createPanel.setAttribute('class', 'info-panel');
+  createPanel.innerText = name;
+  createPanel.style.top = `${y}px`;
+  createPanel.style.left = `${x}px`;
+  return createPanel;
+};
+
+const removeInfoPanel = () => document.querySelector('.info-panel')?.remove();
+
+allEmployees.addEventListener('contextmenu', function (evt) {
+    evt.preventDefault();
+    removeInfoPanel();
+   if (evt.target.getAttribute('class') === 'employee') {
+     const name = evt.target.getAttribute('data-name');
+     const infoPanel = createPanel(evt.clientX - left, evt.clientY - top, name);
+  
+      allEmployees.append(infoPanel);
+   }
+ });
